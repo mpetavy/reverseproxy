c6f25eab09ad6fce675b752dfedd84b5f23e8afc
mpetavy <mpetavy>
2024-12-10 06:09:13 +0100

    Docker image upload

--- build.sh
+++ build.sh
@@ -12,3 +12,7 @@ docker build \
 -f Dockerfile \
 --progress=plain \
 $SRC_PATH
+
+docker login --username $DOCKER_ZEISS_USERNAME --password $DOCKER_ZEISS_PAT
+docker image tag $APP:latest docker.io/$DOCKER_ZEISS_USERNAME/$APP:latest
+docker image push docker.io/$DOCKER_ZEISS_USERNAME/$APP:latest
